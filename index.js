ea36a121ec78e9e59910289ab748279774072e23
root <root@MDKAIF>
2022-04-21 22:59:42 +0600

    fuck you

--- index.js
+++ index.js
@@ -1,31 +1,42 @@
 const discord = require('discord.js');
-const ping = require('./Commands/ping')
+const generateImage = require('./generateImage')
 
  const client = new discord.Client({
     intents: [
         "GUILDS",
-        "GUILD_MESSAGES"
+        "GUILD_MESSAGES",
+        "GUILD_MEMBERS"
     ]
 })
-exports.client = client
+
 
 client.on("ready",()=>{
     console.log(`logged in as ${client.user.tag}`)
-    ping.ping
+
 })
 //ping
 client.on("messageCreate", (event)=>{
-    if (event.content.toLowerCase()==="ping"){
+    if (event.content.toLowerCase()==="!ping"){
         event.reply("Pong!");
     }
 })
 
 
 
-function login(){
+
     client.login("OTI0NTc4NzcwNzg1MDEzODAw.YcgnFA.uPgUyAbJild0DtHPtSnaRltsTng")
-}
-login();
+
+
+const welcomeChannelId ="966699062051090452"
+
+client.on("guildMemberAdd", async (member) => {
+    const img = await generateImage(member)
+    member.guild.channels.cache.get(welcomeChannelId).send({
+        content: `<@${member.id}> Welcome to the server!`,
+        files: [img]
+    })
+})
+
 
 
 
