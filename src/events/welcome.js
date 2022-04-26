9a6477896aabc316019821e994e784ae9ea42258
MdKaif2782 <96629554+MdKaif2782@users.noreply.github.com>
2022-04-27 01:46:04 +0600

    yooyoyo

--- src/events/welcome.js
+++ src/events/welcome.js
@@ -1,12 +1,20 @@
+const discord = require('discord.js');
 module.exports = {
 	name: 'guildMemberAdd',
 	execute(client) {
+		if (!client.guild) return;
     let guild = client.guild
     let channel = guild.channels.cache.find(c => c.name === "welcome");
-    channel.send({content: `Welcome to the server! <@${client.id}>` 
-})
-
+  
+    let membercount = guild.members
+    if (!channel) return;
     
-
+    let embed = new discord.MessageEmbed() 
+      .setColor("GREEN") 
+      .setTitle("New Server Member!")
+      .setDescription(`Welcome, ${client.user.tag} to **${guild.name}!**`)
+      .setThumbnail(client.user.displayAvatarURL())
+      //.setFooter(`You are the ${membercount}th member to join`);
+    channel.send(embed);
 	},
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 const discord = require('discord.js');
 const { TextChannel, Guild, GuildChannel} = require('discord.js')
-const { REST } = require('@discordjs/rest');
-const { Routes } = require('discord-api-types/v9');
 const fs = require('fs')
-
 
  const client = new discord.Client({
     intents: [
@@ -12,32 +9,6 @@ const fs = require('fs')
         "GUILD_MEMBERS"
     ]
 })
-//slash commands handle 
-client.slashCommands = new discord.Collection();
-const commandFiles = fs.readdirSync('src/slashCommands').filter(file => file.endsWith('.js'));
-const slcommands = [];
-for (const file of commandFiles) {
-	const command = require(`./slashCommands/${file}`);
-	client.slashCommands.set(command.data.name, command);
-    slcommands.push(command.data.toJSON());
-}
-client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return;
-
-	const command = client.commands.get(interaction.commandName);
-
-	if (!command) return;
-
-	try {
-		await command.execute(interaction);
-	} catch (error) {
-		console.error(error);
-		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
-	}
-});
-
-
-
 
 
 const prefix = '!'
