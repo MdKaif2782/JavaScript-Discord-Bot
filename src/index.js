82ce912dc76c5bf666df14186244523da8a99f78
MdKaif2782 <96629554+MdKaif2782@users.noreply.github.com>
2022-04-27 01:34:07 +0600

    balsal

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const discord = require('discord.js');
 const { TextChannel, Guild, GuildChannel} = require('discord.js')
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const fs = require('fs')
 
 
  const client = new discord.Client({
@@ -9,8 +12,36 @@ const { TextChannel, Guild, GuildChannel} = require('discord.js')
         "GUILD_MEMBERS"
     ]
 })
+//slash commands handle 
+client.slashCommands = new discord.Collection();
+const commandFiles = fs.readdirSync('src/slashCommands').filter(file => file.endsWith('.js'));
+const slcommands = [];
+for (const file of commandFiles) {
+	const command = require(`./slashCommands/${file}`);
+	client.slashCommands.set(command.data.name, command);
+    slcommands.push(command.data.toJSON());
+}
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
+});
+
+
+
+
+
 const prefix = '!'
-const fs = require('fs')
+
 const path = require('path')
 client.commands= new discord.Collection();
 const commands = fs.readdirSync(path.resolve('src/commands')).filter(file => file.endsWith('.js'));
