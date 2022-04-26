1dfb0cc3217cbacdd9133366c5030c2c71910fee
MdKaif2782 <96629554+MdKaif2782@users.noreply.github.com>
2022-04-26 23:03:20 +0600

    well something for now

--- src/index.js
+++ src/index.js
@@ -65,6 +65,23 @@ client.on("messageCreate", (message)=>{
 
 const welcomeChannelId ="966699062051090452"
 
+client.on('guildMemberAdd', member => {
+    if (!member.guild) return;
+    let guild = member.guild
+    let channel = guild.channels.cache.find(c => c.name === "welcome");
+  
+    let membercount = guild.members
+    if (!channel) return;
+    
+    let embed = new Discord.MessageEmbed() 
+      .setColor("GREEN") 
+      .setTitle("New Server Member!")
+      .setDescription(`Welcome, ${member.user.tag} to **${guild.name}!**`)
+      .setThumbnail(member.user.displayAvatarURL())
+      //.setFooter(`You are the ${membercount}th member to join`);
+    channel.send(embed);
+  });
+
 // client.on("guildMemberAdd", async (member) => {
 //     const img = await generateImage(member)
 //     client.guilds.cache.get(member.guild.id).channels.cache.
