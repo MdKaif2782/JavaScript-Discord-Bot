f7bbd8907c2134e1a30c5bb8f0e9bb1f1cbbba4f
Mdkaif2782 <mibnzaman02@gmail.com>
2025-09-11 19:10:42 +0600

    meme api fixed

--- src/commands/api_commands/meme.js
+++ src/commands/api_commands/meme.js
@@ -1,25 +1,29 @@
-const {MessageActionRow, MessageButton} = require("discord.js");
-module.exports.run = (client,message) => {
-            const { performance } = require('perf_hooks');
+const { MessageActionRow, MessageButton } = require("discord.js");
+module.exports.run = (client, message) => {
+    const { performance } = require('perf_hooks');
     const startTime = performance.now();
     const fetch = require('cross-fetch')
-            const {MessageEmbed} = require('discord.js')
-            async function getMeme(){
-            const response = await fetch('https://meme-api.herokuapp.com/gimme')
-            const data = await response.json()
-            const embed = await new MessageEmbed()
+    const { MessageEmbed } = require('discord.js')
+    async function getMeme() {
+        try{
+        const response = await fetch('https://meme-api.com/gimme')
+        const data = await response.json()
+        const embed = await new MessageEmbed()
             .setTitle(data.title)
-            .setAuthor("Succubot","https://images-ext-2.discordapp.net/external/p1DFrAGKk7cNMjhMZ3D6uoNJAYknMj7jxKk5qx0ArjU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/924578770785013800/a537422edd17a355bd4f8b8cbe37acc4.png?width=468&height=468")
+            .setAuthor("Succubot", "https://images-ext-2.discordapp.net/external/p1DFrAGKk7cNMjhMZ3D6uoNJAYknMj7jxKk5qx0ArjU/%3Fsize%3D1024/https/cdn.discordapp.com/avatars/924578770785013800/a537422edd17a355bd4f8b8cbe37acc4.png?width=468&height=468")
             .setDescription(`*[Posted](${data.postLink}) on r/${data.subreddit} by u/${data.author}*`)
             .setImage(data.url)
-            .setFooter(`Upvotes: ${data.ups}  NSFW: ${data.nsfw}  Spoiler: ${data.spoiler}\nResponse time: ${(performance.now()-startTime).toFixed(1)} ms`)
-            console.log(data);
-                const row = await new MessageActionRow().addComponents(
-                    await new MessageButton().setCustomId('next_meme')
-                        .setLabel("Next Meme")
-                        .setStyle("PRIMARY")
-                )
-                message.channel.send({embeds: [embed],components: [row]});
-            }getMeme();
-
+            .setFooter(`Upvotes: ${data.ups}  NSFW: ${data.nsfw}  Spoiler: ${data.spoiler}\nResponse time: ${(performance.now() - startTime).toFixed(1)} ms`)
+        console.log(data);
+        const row = await new MessageActionRow().addComponents(
+            await new MessageButton().setCustomId('next_meme')
+                .setLabel("Next Meme")
+                .setStyle("PRIMARY")
+        )
+        message.channel.send({ embeds: [embed], components: [row] });
+    } catch(err) {
+        console.log(err.message);
+        message.channel.send(err.message)
+    }
+    } getMeme()
 }
